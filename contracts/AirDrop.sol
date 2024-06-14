3d9f295f357413828c8b77ec1aceee8f986899a2
Anarchist-DEV <168557736+Anarchist-DEV@users.noreply.github.com>
2024-06-14 16:16:38 +0530

    Update AirDrop.sol
    adding sale functionality and went to a user claim method instead of sending asset to the user directly

--- contracts/AirDrop.sol
+++ contracts/AirDrop.sol
@@ -17,34 +17,46 @@ interface IERC1155 {
     function safeTransferFrom(address from, address to, uint256 id, uint256 amount, bytes calldata data) external;
 }
 
-contract BulkAirDrop is Ownable, ReentrancyGuard {
+contract AirDrop is Ownable, ReentrancyGuard {
     constructor(address initialOwner) Ownable(initialOwner) {}
 
     // Define event for ERC20 token transfer
     event ERC20BulkTransfer(address indexed token, address indexed sender, address indexed recipient, uint256 value);
     // Define event for ERC1155 token transfer
     event ERC1155BulkTransfer(address indexed token, address indexed sender, address indexed recipient, uint256 id, uint256 amount);
+    address public acceptedToken; //token addres
+    address public AnarchyToken; //token address
+    uint256 public CF; //conversion factor
 
 
-
-    function bulkAirDropERC20(address _token, address[] calldata _to, uint256[] calldata _value) public onlyOwner nonReentrant {
+    function AirDropERC20(address _token, address _to, uint256 _value) public onlyOwner nonReentrant {
 
         IERC20 token = IERC20(_token);
-        require(_to.length == _value.length, "Receivers and amounts are different length!");
-        for(uint256 i = 0; i < _to.length; i++) {
-            require(token.transferFrom(msg.sender, _to[i], _value[i]),"Transfer failed");
-            emit ERC20BulkTransfer(_token, msg.sender, _to[i], _value[i]);
+
+        token.transferFrom(msg.sender, _to, _value);
+        emit ERC20BulkTransfer(_token, msg.sender, _to, _value);
 
         }
+
+    function bulkAirDrop1155(address _token, address _to, uint256 _id, uint256 _amount) public onlyOwner nonReentrant{
+        IERC1155 token = IERC1155 (_token);
+        token.safeTransferFrom(msg.sender, _to, _id, _amount, '');
+        emit ERC1155BulkTransfer(_token, msg.sender, _to, _id, _amount);   
     }
 
-    function bulkAirDrop1155(address _token, address[] calldata _to, uint256[] calldata _id, uint256[] calldata _amount) public onlyOwner nonReentrant{
-        IERC1155 token = IERC1155 (_token);
-        require(_to.length == _id.length && _to.length == _amount.length, "Receivers, value and amounts are of different length!");
-        for(uint256 i = 0; i < _to.length; i++) {
-            token.safeTransferFrom(msg.sender, _to[i], _id[i], _amount[i], '');
-            emit ERC1155BulkTransfer(_token, msg.sender, _to[i], _id[i], _amount[i]);
+    function BuyANRC(uint256 _value) public nonReentrant{
+        IERC20 token = IERC20(acceptedToken);
+        IERC20 ANRC = IERC20(AnarchyToken);
+        token.transferFrom(msg.sender, owner(), _value);
+        uint256 amount = _value*CF/10^18;//conversion Factor and dividing 10^18
+        ANRC.transferFrom(owner(), msg.sender, amount);  
 
-        }
+    }
+
+    function setAcceptedToken(address _tokenAddress) public onlyOwner nonReentrant {
+        acceptedToken = _tokenAddress;
+    }
+    function setConversionFactor(uint256 _cf) public onlyOwner nonReentrant {
+        CF = _cf; //always put value conversion factor * 10^18
     }
 }
